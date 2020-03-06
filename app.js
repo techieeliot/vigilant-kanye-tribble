68871a747b4e7a1d027ffb1fcee374aed45de27c
levi <levi@mscode.dev>
2020-03-05 20:49:47 -0600

    feat: add favorite quote functionality

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const mysql    = require('mysql')
 const app      = express()
 const port     = 3000
 const KanyeURL = 'http://api.kanye.rest'
+let currentQuote = ''
 
-const conn     = mysql.createConnection({
+const con      = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'kanye'
 })
+con.connect(err => console.log(err))
 
-const getKanyeWrapper = (wisdom) => `<h1>${wisdom}</h1>`
+
+const getKanyeWrapper = (wisdom) => `<h1>${wisdom}</h1><button onClick="addWisdom()">Add</button><script>function addWisdom(){ fetch('/add') }</script>`
 
 const getKanyeQuoteCallback = (response) => (err, res, body) => {
     if(err) return console.log(err)
+    currentQuote = body.quote
     response.send(getKanyeWrapper(body.quote))
 }
 
@@ -24,6 +29,25 @@ const kanyeFetcher = (req, response) => {
     return request(KanyeURL, {json: true}, KanyeCallback)
 }
 
+const addQuote = (quote) => {
+    const queryString = `insert into quotes(quote,created_at) values("${quote}", now());`
+    con.query(queryString, (err, results, fields) => {
+    if(err) console.log(err)
+    })
+}
+
 app.get('/', kanyeFetcher)
+
+app.get('/add', (req,res) => {
+    addQuote(currentQuote)
+    res.sendStatus(200)
+})
+
+app.get('/best', (req, res) => {
+    con.query('select * from quotes where deleted_at is null order by created_at desc;', (err, results, fields)=>{
+        if(err) console.log(err)
+        res.send(`<h1>${results[0].quote}</h1>`)
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
