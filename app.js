9bb3400501d4a0ddca55b3b85028264dd08f3bfd
Garrick Crouch <services@localpathcomputing.com>
2020-03-10 18:40:49 -0500

    add route for sign-up form in server.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express  = require('express')
-const request  = require('request')
-const mysql    = require('mysql')
-const app      = express()
-const port     = 3000
+const express = require('express')
+const request = require('request')
+const mysql = require('mysql')
+const app = express()
+const port = 3000
 const KanyeURL = 'http://api.kanye.rest'
 let currentQuote = ''
 
-const con      = mysql.createConnection({
+const con = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -24,7 +24,7 @@ const getKanyeWrapper = (wisdom) => `
 `
 
 const getKanyeQuoteCallback = (response) => (err, res, body) => {
-    if(err) return console.log(err)
+    if (err) return console.log(err)
     currentQuote = body.quote
     response.send(getKanyeWrapper(body.quote))
 }
@@ -32,30 +32,32 @@ const getKanyeQuoteCallback = (response) => (err, res, body) => {
 const kanyeFetcher = (req, response) => {
     const KanyeCallback = getKanyeQuoteCallback(response)
 
-    return request(KanyeURL, {json: true}, KanyeCallback)
+    return request(KanyeURL, { json: true }, KanyeCallback)
 }
 
 const addQuote = (quote) => {
     const queryString = `insert into quotes(quote,created_at) values("${quote}", now());`
     con.query(queryString, (err, results, fields) => {
-    if(err) console.log(err)
+        if (err) console.log(err)
     })
 }
 
 const getBest = (req, res) => {
-    con.query('select * from quotes where deleted_at is null order by created_at desc;', (err, results, fields)=>{
-        if(err) console.log(err)
+    con.query('select * from quotes where deleted_at is null order by created_at desc;', (err, results, fields) => {
+        if (err) console.log(err)
         res.send(`<h1>${results[0].quote}</h1>`)
     })
 }
 
 app.get('/', kanyeFetcher)
 
-app.get('/add', (req,res) => {
+app.get('/add', (req, res) => {
     addQuote(currentQuote)
     res.sendStatus(200)
 })
 
 app.get('/best', getBest)
+
+app.get('/sign-up', (req, res) => { res.sendFile(__dirname + '/views/sign-up.html') })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
