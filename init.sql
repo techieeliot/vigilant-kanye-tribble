3644369967bf8ecd79153801827ded82bfedcb0a
levi <levi@mscode.dev>
2020-03-10 19:59:23 -0500

    fix: correct syntax errors in init.sql
    - init.sql had create table users below create table quotes
    this causes an error because quotes has a foreign key on users
    - fixes typo where users was user (non plural) defying convention
    - adds missing user_id field in create table quotes

--- init.sql
+++ init.sql
@@ -6,16 +6,7 @@ grant all PRIVILEGES on *.* to 'kanye'@'localhost';
 
 use kanye;
 
-create table quotes(
-    id int auto_increment,
-    quote varchar(240),
-    created_at datetime,
-    deleted_at datetime,
-    primary key(id),
-    foreign key(user_id)
-);
-
-create table user(
+create table users(
     id int auto_increment,
     profile_name varchar(16),
     password varchar(20),
@@ -27,6 +18,7 @@ create table user(
 =======
 );
 
+<<<<<<< HEAD
 create table user(
     id int auto_increment,
     profile_name varchar(16),
@@ -36,4 +28,14 @@ create table user(
     deleted_at datetime,
     primary key(id)
 >>>>>>> 97ea2ff... feat: create user table
+=======
+create table quotes(
+    id int auto_increment,
+    user_id int,
+    quote varchar(240),
+    created_at datetime,
+    deleted_at datetime,
+    primary key(id),
+    foreign key(user_id) references users(id)
+>>>>>>> d411c30... fix: corrects syntax errors in init.sql
 );
